1407385b303b0da675283deda04c02539e662ea1
Ali Khan <alik@robarts.ca>
2023-03-03 20:44:22 -0500

    added files for analyzing data for revision

---
+++ revision_compare/README.md
@@ -0,0 +1,5 @@
+Workflow for generating figures/data for the revision, making use of the masks in this repo, and the original datasets (i.e. not requiring a re-run of the segmentation)..
+
+
+1. extract `data_funcmasker.tar` into this folder (from: `~/graham/ext-data/fetal_bold/data_funcmasker.tar`) 
+
---
+++ revision_compare/scripts/calc_metrics_4d.py
@@ -0,0 +1,35 @@
+import nibabel as nib
+import numpy as np
+import pandas as pd
+
+manual_nib = nib.load(snakemake.input.manual)
+auto_nib = nib.load(snakemake.input.auto)
+
+
+manual_vols = manual_nib.get_fdata()
+auto_vols = auto_nib.get_fdata()
+
+print(manual_vols.shape)
+
+out_dict={'id':[],'sensitivity':[],'specificity':[]}
+
+scanname=snakemake.params.scanname
+
+for i in range(manual_vols.shape[3]):
+    manual_3d = manual_vols[:,:,:,i]
+    auto_3d = auto_vols[:,:,:,i]
+
+    tp=np.logical_and(manual_3d>0,auto_3d>0).sum()
+    tn=np.logical_and(manual_3d==0,auto_3d==0).sum()
+    fp=np.logical_and(manual_3d==0,auto_3d>0).sum()
+    fn=np.logical_and(manual_3d>0,auto_3d==0).sum()
+
+    
+    out_dict['sensitivity'].append(tp/(tp+fn))
+    out_dict['specificity'].append(tn/(tn+fp))
+    out_dict['id'].append(f'{scanname}-{i}')
+
+print(out_dict)
+df = pd.DataFrame(out_dict)
+
+df.to_csv(snakemake.output.csv,index=False)
